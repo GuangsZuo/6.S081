56c1a21e0ee5bdb26c0a54bb536541962f39755e
guzuo <qf.zgs.1994@gmail.com>
2021-08-25 23:19:54 +0800

    format code

--- user/find.c
+++ user/find.c
@@ -3,40 +3,52 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-int contains(char* path, char* reg) {
-	for (int i=0; i<=strlen(path)-strlen(reg); ++i) {
-		char* p = path + i, *q = reg;
-		while (*q && *q==*p) q++, p++;
-		if (*q=='\0') return 1;
-	}	 
-	return 0; 
+int contains(char *path, char *reg)
+{
+	for (int i = 0; i <= strlen(path) - strlen(reg); ++i)
+	{
+		char *p = path + i, *q = reg;
+		while (*q && *q == *p)
+			q++, p++;
+		if (*q == '\0')
+			return 1;
+	}
+	return 0;
 }
-void find(char* path, char* reg) {
+void find(char *path, char *reg)
+{
 	int fd;
 	struct stat st;
-	if ((fd=open(path,0)) < 0) {
-		fprintf(2, "error path: %s\n",path);
-		return ;
+	if ((fd = open(path, 0)) < 0)
+	{
+		fprintf(2, "error path: %s\n", path);
+		return;
 	}
 	fstat(fd, &st);
 	//fprintf(1, "%s %d %d\n", path, fd, st.type);
-	if (st.type == T_FILE) {
-		if (contains(path, reg)) fprintf(1, "%s\n", path);
-	} else if (st.type == T_DIR) {
+	if (st.type == T_FILE)
+	{
+		if (contains(path, reg))
+			fprintf(1, "%s\n", path);
+	}
+	else if (st.type == T_DIR)
+	{
 		char buf[256];
 		struct dirent de;
 		strcpy(buf, path);
-		char* p = buf + strlen(path);
+		char *p = buf + strlen(path);
 		*p++ = '/';
-		while (read(fd, &de, sizeof(de)) == sizeof(de)) {
-			if (de.inum == 0){
+		while (read(fd, &de, sizeof(de)) == sizeof(de))
+		{
+			if (de.inum == 0)
+			{
 				continue;
 			}
-			if (strcmp(de.name, ".") ==0 || 
-					strcmp(de.name, "..")==0) 
+			if (strcmp(de.name, ".") == 0 ||
+				strcmp(de.name, "..") == 0)
 				continue;
 			//fprintf(1, "%s\n", de.name);
-			strcpy(p,de.name);
+			strcpy(p, de.name);
 			p[DIRSIZ] = 0;
 			//fprintf(1,"%s\n",buf);
 			find(buf, reg);
@@ -45,15 +57,14 @@ void find(char* path, char* reg) {
 	close(fd);
 }
 
-int
-main(int argc, char *argv[])
-{	
-	char reg[32]="\0";
-	char path[256]=".";	
-	if (argc>1)
-	strcpy(path,argv[1]); 
-	if (argc>2)
-	strcpy(reg,argv[2]);
+int main(int argc, char *argv[])
+{
+	char reg[32] = "\0";
+	char path[256] = ".";
+	if (argc > 1)
+		strcpy(path, argv[1]);
+	if (argc > 2)
+		strcpy(reg, argv[2]);
 	find(path, reg);
 	exit(0);
-} 
+}
--- user/primes.c
+++ user/primes.c
@@ -2,29 +2,37 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-
-void newpipeline(int *p) {
+void newpipeline(int *p)
+{
 	int q[2];
-	int x ;
+	int x;
 	int status = read(p[0], &x, 4);
 	//fprintf(1,"status:%d\n", status);
-	if (!status) return;
+	if (!status)
+		return;
 	pipe(q);
-	if (status!=0 && fork()==0) {
+	if (status != 0 && fork() == 0)
+	{
 		close(q[1]);
 		newpipeline(q);
 	}
-	else {
-		int  fx=-1;
+	else
+	{
+		int fx = -1;
 		close(q[0]);
-		while (status!=0) {
-			if (fx<0) {
+		while (status != 0)
+		{
+			if (fx < 0)
+			{
 				fx = x;
-				fprintf(1, "prime %d\n",fx);
-			} else {
-				if (x % fx!=0) {
-				write(q[1],&x, 4);
-				//fprintf(1,"%d\n",x);
+				fprintf(1, "prime %d\n", fx);
+			}
+			else
+			{
+				if (x % fx != 0)
+				{
+					write(q[1], &x, 4);
+					//fprintf(1,"%d\n",x);
 				}
 			}
 			status = read(p[0], &x, 4);
@@ -35,17 +43,20 @@ void newpipeline(int *p) {
 	}
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int p[2];
 	pipe(p);
-	if (fork()==0) {
+	if (fork() == 0)
+	{
 		close(p[1]);
-		newpipeline(p);	
-	} else {
+		newpipeline(p);
+	}
+	else
+	{
 		close(p[0]);
-		for (int i=2;i<=35; ++i) write(p[1], &i, 4);
+		for (int i = 2; i <= 35; ++i)
+			write(p[1], &i, 4);
 		close(p[1]);
 		wait(0);
 	}
